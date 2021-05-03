910a14da5618565b507ef88bda7df7b31f7ef773
Arevik Hayrapetyan <arev.hayrapetyan99@mail.ru>
2021-05-03 16:59:12 +0400

    feat: ParseInt logic

---
+++ bootcamp-task05/parseFloat.js
@@ -0,0 +1,15 @@
+let input = "125.6a";
+let parse = "";
+if (!Number(input[0])) {
+  console.log(NaN);
+} else {
+  for (i = 0; i < input.length; i++) {
+    if (isNaN(input[i])) {
+        break
+
+    } else {
+      parse += input[i];
+    }
+  }
+}
+console.log(Number(parse));
