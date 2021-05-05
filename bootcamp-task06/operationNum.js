36465a7a5b88e89b2226e611f7e79f0f5570a81f
Arevik Hayrapetyan <arev.hayrapetyan99@mail.ru>
2021-05-05 21:48:08 +0400

    feat: Sum, Difference, Multiply, Devision operations:

--- bootcamp-task06/operationNum.js
+++ bootcamp-task06/operationNum.js
@@ -1,4 +1,4 @@
-function sumNum(elem1, elem2) {
+function findSumNum(elem1, elem2) {
   let array = [4, 5, 10, 8, 1];
   if (elem1 > array.length - 1 || elem2 > array.length - 1) {
     return "Error";
@@ -6,7 +6,7 @@ function sumNum(elem1, elem2) {
   return array[elem1] + array[elem2];
 }
 
-function differenceNum(elem1, elem2) {
+function findDifferenceNum(elem1, elem2) {
   let array = [4, 5, 10, 8, 1];
   if (elem1 > array.length - 1 || elem2 > array.length - 1) {
     return "Error";
@@ -14,14 +14,14 @@ function differenceNum(elem1, elem2) {
   return array[elem1] - array[elem2];
 }
 
-function multiNum(elem1, elem2) {
+function findMultiNum(elem1, elem2) {
   let array = [4, 5, 10, 8, 1];
   if (elem1 > array.length - 1 || elem2 > array.length - 1) {
     return "Error";
   }
   return array[elem1] * array[elem2];
 }
-function divisionNum(elem1, elem2) {
+function findDivisionNum(elem1, elem2) {
   if (array[elem2] === 0) {
     return "The result is infinity";
   }
