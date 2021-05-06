e75f0bd258efc7385e2ec7b16f43e4eb5ab4c49c
Arevik Hayrapetyan <arev.hayrapetyan99@mail.ru>
2021-05-06 09:54:36 +0400

    feat: Compute new string

--- bootcamp-task06/computeNewString.js
+++ bootcamp-task06/computeNewString.js
@@ -1,3 +1,11 @@
-function computeNewString (){
-    
+function computeNewString(input) {
+  let result = "";
+  let length = input.length % 3;
+
+  for (let i = 0; i < input.length - length; i += 3) {
+    result += input.slice(i + 1, i + 3) + input[i];
+  }
+  return result + input.slice(input.length - length);
 }
+
+console.log(computeNewString("abcdfgaa"));
