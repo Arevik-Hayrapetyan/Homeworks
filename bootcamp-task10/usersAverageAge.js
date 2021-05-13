2c734b2475c7645c6d8d76ca80f1ba8572a3f131
Arevik Hayrapetyan <arev.hayrapetyan99@mail.ru>
2021-05-13 23:34:33 +0400

    feat: Adding tasks

--- bootcamp-task10/usersAverageAge.js
+++ bootcamp-task10/usersAverageAge.js
@@ -1,20 +1,20 @@
 function usersAverageAge(users) {
-    let averageAge = users.reduce((total, age) => {
-      return total + age.age / users.length;
-    }, 0);
-    return Math.round(averageAge);
-  }
-  const users = [
-    {
-      username: "Yuri Gagarin",
-      lang: "ru",
-      age: 56,
-    },
-    {
-      username: "Nil Armstrong",
-      lang: "ENG",
-      age: 54,
-    },
-  ];
-  
-  console.log(usersAverageAge(users));
+  let averageAge = users.reduce((total, age) => {
+    return total + age.age / users.length;
+  }, 0);
+  return Math.round(averageAge);
+}
+const users = [
+  {
+    username: "Yuri Gagarin",
+    lang: "ru",
+    age: 56,
+  },
+  {
+    username: "Nil Armstrong",
+    lang: "ENG",
+    age: 54,
+  },
+];
+
+console.log(usersAverageAge(users));
