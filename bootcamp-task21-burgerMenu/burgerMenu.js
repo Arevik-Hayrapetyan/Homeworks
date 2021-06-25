0980175930146da2944f9dd735dd17532ab94d1b
Arevik Hayrapetyan <arev.hayrapetyan99@mail.ru>
2021-06-25 16:49:47 +0400

    feat: Notification and Burger-Menu task

--- bootcamp-task21-burgerMenu/burgerMenu.js
+++ bootcamp-task21-burgerMenu/burgerMenu.js
@@ -1,5 +1,8 @@
 const button = document.querySelector(".fa-bars");
+const inboxButton = document.querySelector(".inboxButton");
+
 button.addEventListener("click", menuFunction);
+inboxButton.addEventListener("click", noteFunction);
 
 function menuFunction(event) {
   const menu = document.querySelector(".menu-links");
@@ -8,5 +11,30 @@ function menuFunction(event) {
     menu.style.display = "none";
   } else {
     menu.style.display = "block ";
+  }
+}
+
+function noteFunction(event) {
+  const noteContainer = document.querySelector(".note-container");
+  const newDiv = document.createElement("div");
+  const noteText = document.createElement("p");
+  const closeButton = document.createElement("button");
+  const wrapperDiv = document.querySelector(".wrapper");
+
+  newDiv.classList.add("note-container");
+  noteText.classList.add("noteText");
+  closeButton.classList.add("closeButton");
+
+  noteText.innerText = "Hello from Caxkadzor";
+  closeButton.innerHTML = '<i class="fas fa-window-close"></i>';
+  closeButton.addEventListener("click", closeNote);
+
+  newDiv.style.display = "block";
+  newDiv.append(closeButton);
+  newDiv.append(noteText);
+  wrapperDiv.append(newDiv);
+
+  function closeNote(event) {
+    newDiv.style.display = "none";
   }
 }
