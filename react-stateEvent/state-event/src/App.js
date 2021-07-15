f75f82298f587f8b7f5f180caa837c3995f3033f
Arevik Hayrapetyan <arev.hayrapetyan99@mail.ru>
2021-07-15 16:02:23 +0400

    fix(Calculator.jsx): Changing folder name

--- react-stateEvent/state-event/src/App.js
+++ react-stateEvent/state-event/src/App.js
@@ -1,19 +1,11 @@
 
 import './App.css';
-import { Button } from './Components/Input';
+import { Calculator } from './Components/Calculator';
 
 function App() {
   return (
     <div className="App">
-      <Button></Button>
-      {/* increaseButton */}
-      {/* decreaseButton */}
-      {/* reset for 0 */}
-      {/* decrease must not work for 0 */}
-      {/* input
-      1.for maximal number
-      2. for minimal  number
-      3. step */}
+      <Calculator></Calculator>
     </div>
   );
 }
--- react-stateEvent/state-event/src/Components/Calculator.jsx
+++ react-stateEvent/state-event/src/Components/Calculator.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export class Button extends React.Component {
+export class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
